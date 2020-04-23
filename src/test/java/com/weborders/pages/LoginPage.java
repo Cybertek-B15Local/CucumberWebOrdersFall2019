5f1410e5d612a8571c6356752a61bf5553215519
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-22 21:46:42 -0400

    utils

--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -1,4 +1,28 @@
 package com.weborders.pages;
 
+import com.weborders.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy(id = "ctl00_MainContent_username")
+    private WebElement userName;
+
+    @FindBy(id = "ctl00_MainContent_password")
+    private WebElement password;
+
+    public void login() {
+        String usernameValue = ConfigurationReader.getProperty("username");
+        String passwordValue = ConfigurationReader.getProperty("password");
+        userName.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+    public void login(String usernameValue, String passwordValue) {
+        userName.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
 }
--- src/test/java/com/weborders/step_definitions/Hooks.java
+++ src/test/java/com/weborders/step_definitions/Hooks.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+
     @Before
     public void setup(Scenario scenario) {
         System.out.println(":::: Starting test automation ::::");
